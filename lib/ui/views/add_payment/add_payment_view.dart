d4472e0850f88e404cebb676d36e6ca9eb22132b
edoliantesjr <edoliantesjr@gmail.com>
2022-05-15 10:25:19 +0800

    init code for add payment module

---
+++ lib/ui/views/add_payment/add_payment_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddPaymentView extends StatelessWidget {
+  const AddPaymentView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //todo: implement add payment view
+    return Container();
+  }
+}
---
+++ lib/ui/views/add_payment/add_payment_view_model.dart
@@ -0,0 +1,5 @@
+import 'package:stacked/stacked.dart';
+
+class AddPaymentViewModel extends BaseViewModel {
+//  todo: logic code for add payment view model
+}
